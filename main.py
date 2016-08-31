c7bfffea6e4a3a110019d5eeda46e3f43b1ea513
KevinStock <kevin@kevinstock.net>
2016-08-30 19:50:36 -0500

    blog complete without styling

--- main.py
+++ main.py
@@ -15,11 +15,68 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Posts(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    date = db.DateProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_blog(self):
+        posts = db.GqlQuery("SELECT * FROM Posts ORDER BY date DESC")
+        self.render("blog.html", posts = posts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_blog()
+
+class NewPost(Handler):
+    def render_form(self, subject="", content="", error=""):
+        self.render("newpost.html", subject = subject, content = content, error = error)
+
+    def get(self):
+        self.render_form()
+
+    def post(self):
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+
+        if subject and content:
+            p = Posts(subject = subject, content = content)
+            p.put()
+
+            self.redirect("/%s" % str(p.key().id()))
+        else:
+            error = "subject and content required"
+            self.render_form(subject, content, error)
+
+class PermaLink(Handler):
+    def get(self, post_id):
+        post = Posts.get_by_id(int(post_id))
+
+        self.render("permalink.html", subject = post.subject,
+                    content = post.content, date = post.date)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    ('/newpost', NewPost),
+    ('/(\d+)', PermaLink)
 ], debug=True)
